b33ce23ba85506c26124cf2d2d63c5642eaca550
bmwtsn098 <bmwtsn99@gmail.com>
2022-10-21 23:57:45 -0700

    add GitHub action to launch Mayhem

---
+++ mayhem/fuzz.go
@@ -0,0 +1,39 @@
+package fuzz
+
+import "strconv"
+import "github.com/ikilobyte/netman/server"
+import "github.com/ikilobyte/netman/util"
+
+func mayhemit(bytes []byte) int {
+
+    var num int
+    if len(bytes) < 1 {
+        num = 0
+    } else {
+        num, _ = strconv.Atoi(string(bytes[0]))
+    }
+
+    switch num {
+
+    case 1:
+        content := string(bytes)
+        server.WithTls("mayhem", content)
+        return 0
+
+    case 2:
+        var test util.DataPacker
+        test.UnPack(bytes)
+        return 0
+
+    default:
+        var test util.DataPacker
+        num := uint32(num)
+        test.Pack(num, bytes)
+        return 0
+    }
+}
+
+func Fuzz(data []byte) int {
+    _ = mayhemit(data)
+    return 0
+}
